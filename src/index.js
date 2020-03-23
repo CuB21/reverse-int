ab791171ce36c0c96ae52153b0f29ba27f0487b1
CuB21 <igor.yakubovsky@gmail.com>
2020-03-23 23:49:28 +0300

    feat:add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+  const string = n.toString();
+  const array = Array.from(string);
   
+  const newArray = array.reverse();
+  const newString = newArray.join('');
+  return parseInt(newString);
 }
